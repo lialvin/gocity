06b0dc1dcf289444ddb3cdfdd89d1ddb3724ea1b
alvin <li_alvin@qq.com>
2019-07-18 18:11:53 +0800

    3

--- test_uos/main.go
+++ test_uos/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
-
+    "os"
+    "bufio"
+    "strconv"
+    "strings" 
 )
 
 func getAPIURL() string {
@@ -16,17 +18,73 @@ func getAPIURL() string {
 }
 
 
-func main() {
+func  display() {
+    fmt.Printf("read from file: 1 \n")
+    fmt.Printf("write to  file : 2 \n")
+    fmt.Printf("sync from uos node: 3 \n")   
+    fmt.Printf("statistic uid top 10: 4 \n")    
+    fmt.Printf("print uid info: 5 \n")    
+    fmt.Printf("exit : 6 \n")    
+}
+ 
+func getinput() string{
+    inputReader := bufio.NewReader(os.Stdin)
+    input, _ := inputReader.ReadString('\n')    
+    keyval:= strings.TrimSpace(input)
+    return keyval
+}
+
+
+func  getTime()  int {
+    keyval :=getinput()
+    time, _:=  strconv.Atoi(keyval) 
+    return time 
+}
+
+
+
+func main() {    
+    
+    // fmt.Printf("Your name is %s", input)
+    uidSysObj := new(UidSys)
+    for {
+        display()
+        inputReader := bufio.NewReader(os.Stdin)
+        input, _ := inputReader.ReadString('\n')    
+        keyval:= strings.TrimSpace(input)
+        //fmt.Println("input", input )
+        switch keyval {
+            case "1" :
+                fmt.Println("read from file !")
+                uidSysObj.ReadfromFile("D:\\uidsystemdata.txt")                   
+            case "2":
+                fmt.Println("write to  file  !")
+                uidSysObj.WritetoFile("D:\\uidsystemdata.txt")                                
+            case "3":
+                fmt.Println("sync from uos node")
+                uidSysObj.SyncUid("1000")
+            case "4":
+                startTime:= getTime()
+                endTime:= getTime()
+                fmt.Println("stat from uos node")                
+                uidSysObj.StatByTime(startTime ,endTime )               
+            case "5":
+                uid:=getinput()
+                uidSysObj.printuid(uid)
+            case "6":    
+                fmt.Println("exit sysyem")
+                uidSysObj.WritetoFile("D:\\uidsystemdata.txt") 
+                os.Exit(0)
+        }
+    }
+
 
     fmt.Println("start read from file ")
     //fmt.Println("info rest:",infoResp)
-    //fmt.Println("Permission for uosio:", accountResp.Permissions[0].RequiredAuth.Keys)
+    //fmt.Println("Permission for uosio:", accountResp.Permissions[0].RequiredAuth.Keys)  
     
-    uidSysObj := new(UidSys)
-    uidSysObj.ReadfromFile()
     // map[string][]UidSystem , map[int][]UidAccount
 
+    // 增加新功能，统计 新增用户 。 一期(区块周)  每一期 为 7*24*3600  . 统计团体新增用户 从某一秒开始统计   
+    // 读取表， 生成记录 本地存储  map[] [] 
 }
-
-// 增加新功能，统计 新增用户 。 一期(区块周)  每一期 为 7*24*3600  . 统计团体新增用户 从某一秒开始统计   
-// 读取表， 生成记录 本地存储  map[] [] 
--- test_uos/uidrw.go
+++ test_uos/uidrw.go
@@ -37,106 +37,201 @@ type UidSystem struct {
 
 
 type  UidSys   struct {    
-	UidSystemData      map[string]UidSystem   `json:"uidsystemdata"`
-	UidAccountData     map[int][]UidAccount     `json:"uidaccountdata"` 
+	UidSystemData      map[string]UidSystem    `json:"uidsystemdata"`
+	UidAccountData     map[string]UidAccount   `json:"uidaccountdata"` 
+	UidTimeData        map[int][]UidSystem     `json:"uidtimedata"` 
+	sslice             []int                   `json:"uidsloce"` 
+	startresult        map[string]int          `json:"startresult"` 
 }
 
 
-func IsAccount( ) {
+func IsAccount( account_name string )  int{
 	api := uos.New(getAPIURL())
 
-	account := uos.AccountName("uosuidwallet")
+	account := uos.AccountName(account_name)
 	info, err := api.GetAccount(account)
 	if err != nil {
 		if err == uos.ErrNotFound {
-			fmt.Printf("unknown account: %s", account)
-			return
+			fmt.Printf("unknown account: %s", account_name)
+			return 0
 		}
 
-		panic(fmt.Errorf("get account: %s", err))
+		return IsAccount(account_name)    
+		//panic(fmt.Errorf("get account: %s", err))
 	}
 
 	bytes, err := json.Marshal(info)
 	if err != nil {
-		panic(fmt.Errorf("json marshal response: %s", err))
+		//return 0xff
+		//panic(fmt.Errorf("json marshal response: %s", err))
 	}
-
+	
 	fmt.Println(string(bytes))
+	return 1
 }
 
-// 获取账户表
-func  (uidSys *UidSys) GetUosUidAccount(starttime  string ){
 
+// 获取账户表 starttime = "156344000"
+func  (uidSys *UidSys) GetUosUidAccount(starttime  string ) (int , int ){
     var out []UidAccount
 
 	api := uos.New("https://testrpc1.uosio.org:20580")
     // -L starttime 最小值是 starttime
-    sql := uos.GetTableRowsRequest{"uosuidwallet","uosuidwallet" , "uidaccount", starttime, "", 20,  "i64", "2", "", true} 
+    sql := uos.GetTableRowsRequest{"uosuidwallet","uosuidwallet" , "uidaccount", starttime, starttime, 50,  "i64", "2", "", true} 
         
     //infoResp, _ := api.GetInfo()    
     inforow1, _err := api.GetTableRows(sql)
 	err2:=  inforow1.JSONToStructs( &out )
-	
-	fmt.Println(" uidaccount len   ", len(out) ,_err, err2  )
-	//for( )
-	
+	count:= len(out)
+	fmt.Println(" uidaccount len   ", count ,_err, err2  )
+	maxTime :=0
+	for  _ , uidobj := range out {
+		if IsAccount(uidobj.Uid)== 1 {			
+			_, ok  := uidSys.UidAccountData[uidobj.Uid]		
+			if !ok {
+				uidSys.UidAccountData[uidobj.Uid] = uidobj	
+				uidSys.GetUosUidSystem(uidobj.Uid,uidobj.Creattime );					
+				if uidobj.Creattime >maxTime{
+					maxTime= uidobj.Creattime 
+					uidSys.sslice = append(uidSys.sslice, maxTime)
+				}
+			}	
+		}
+	}
+	return maxTime ,count
+}
+// 按时间统计 
+func (uidSys *UidSys)  StatByTime(startTime int ,endTime int) {
+	//UidTimeData[uidobj.Creattime] =  
+	for   _, idx := range uidSys.sslice {
+		if uidSys.sslice[idx] >= startTime && uidSys.sslice[idx] < endTime  {
+			keyTime:= uidSys.sslice[idx] 
+			for  _,obj := range uidSys.UidTimeData[keyTime] {				
+				uidSys.startresult[obj.Highuid] = uidSys.startresult[obj.Highuid]+1 
+			}
+		}		
+	}
 }
 
-func (uidSys *UidSys) GetUosUidSystem(){
+func (uidSys *UidSys) GetUosUidSystem(account_name string, creattime int  ){
     var out []UidSystem
 
-    s1 := uos.GetTableRowsRequest{"uosuidwallet","uosuidwallet" , "uidsystem", "asasas121212", "asasas121212", 1,  "i64", "1", "", true} 
+    s1 := uos.GetTableRowsRequest{"uosuidwallet","uosuidwallet" , "uidsystem",account_name, account_name, 1,  "i64", "1", "", true} 
     
     api := uos.New("https://testrpc1.uosio.org:20580")
     //infoResp, _ := api.GetInfo()
-    IsAccount();
+    
     inforow1, _err := api.GetTableRows(s1)
 
     err2:=  inforow1.JSONToStructs( &out )
 
+	if  len(out) !=1  {
+		fmt.Println(" uiduoswallet len err  ", len(out) )    	
+	}
 
-    fmt.Println(" uiduoswallet len   ", len(out) )    
+	uidSys.UidSystemData[account_name] = out[0] 
+	uidSys.UidTimeData[creattime] = append(uidSys.UidTimeData[creattime], out[0])
+    
         
     //fmt.Println("info rest:",string(inforow1.Rows))
     fmt.Println("uiduoswallet table:",out[0],err2, _err )
 }
 
-// map[string][]string
-func (uidSys *UidSys)  ReadfromFile() {
-    b, err := ioutil.ReadFile("D:\\whitelist.csv")
-    if err != nil {
-		fmt.Print(err)
-		return 
-    }
-
-    whitelist := map[string]bool{}
-    err = json.Unmarshal([]byte(string(b)), &whitelist)
-    if err != nil {
-        panic(err)
-    }
-
-    for key, value := range whitelist {
-        fmt.Println("key", key, "value", value)
-        fmt.Printf("%T", value)
-    }
+func checkFileIsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
 }
 
-func (uidSys *UidSys) WritetoFile( data [] byte) {
-    err := ioutil.WriteFile("D:\\whitelist.csv",data,os.ModeAppend)
+// map[string][]string  "D:\\uidaccountdata.txt"
+func (uidSys *UidSys)  ReadfromFile(filename string) int {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+		fmt.Println("file exist start read :",exist)
+		return  0
+	}
+	
+    buf, err := ioutil.ReadFile(filename)
+    if err != nil {
+		fmt.Print(err)
+		return  0
+    }
+
+    //whitelist := map[string]bool{}
+	//err = json.Unmarshal([]byte(string(buf)), &whitelist)
+	err = json.Unmarshal([]byte(string(buf)), &uidSys.UidAccountData)	
+    if err != nil {
+        panic(err)
+	}
+	
+	var maxkey =0 
+	
+	for  _  , key    := range uidSys.UidAccountData {
+		if key.Creattime >maxkey {
+		    maxkey= key.Creattime
+		}		
+	}
+	fmt.Println("max key is :",maxkey)
+	// 从 maxkey +1 查找新的key 
+	return  maxkey
+}
+
+func (uidSys *UidSys) WritetoFile( filename string) {
+	//var buf []byte 
+	buf , _err := json.Marshal( uidSys.UidAccountData )
+	
+	if  _err != nil  {
+		fmt.Print(" Marshal UidAccountData err: ", _err)
+	}
+	     
+    err := ioutil.WriteFile(filename,buf,os.ModeAppend)
     if err != nil {
         fmt.Print(err)
     }
+}
+// 从网络读取
+func (uidSys *UidSys)  SyncUid(startTime  string  ){
+	//var buf []byte 
+	newUidTime,count := uidSys.GetUosUidAccount("1000")
+	for {   
+		if count <50{
+			break;	
+		} 
+		newUidTime,count  = uidSys.GetUosUidAccount(string(newUidTime))	
+	}
+		
 
-    whitelist := map[string]bool{}
-    //err = json.Unmarshal([]byte(string(b)), &whitelist)
-    if err != nil {
-        panic(err)
-    }
-
-    for key, value := range whitelist {
-        fmt.Println("key", key, "value", value)
-        fmt.Printf("%T", value)
-    }
+	fmt.Println("max time is :",newUidTime)
 }
 
 
+func (uidSys *UidSys)  printuid(uidstd string){
+
+	var out []UidSystem
+	
+	uid:=uidstd+" "
+    s1 := uos.GetTableRowsRequest{"uosuidwallet","uosuidwallet" , "uidsystem",uid, uid, 1,  "i64", "1", "", true} 
+    //s1 := uos.GetTableRowsRequest{"uosuidwallet","uosuidwallet" , "uidsystem","", "", 10,  "i64", "1", "", true} 
+    api := uos.New("https://testrpc1.uosio.org:20580")
+    //infoResp, _ := api.GetInfo()
+    
+    inforow1, _err := api.GetTableRows(s1)
+	fmt.Println("uidsystem test uid:",string(inforow1.Rows) )
+    err2:=  inforow1.JSONToStructs( &out )
+	
+	if  len(out) !=1  {
+		fmt.Println(" uidsystem   len=0,  find err  ",uid, len(out),_err ,err2)    	
+		return 
+	}   
+        
+ 
+	fmt.Println("\n\n")
+}
+
+func (uidSys *UidSys)  StatUid(){
+
+	
+}
